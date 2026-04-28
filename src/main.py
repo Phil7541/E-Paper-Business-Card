017d14d006d003e203a1668e89deec639056f0f6
Phil7541 <phillipswann2008@gmail.com>
2026-04-28 17:55:06 +0100

    Hpefully finished and cleanup

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 import sys
 import os
-from datetime import datetime
 import time
 import logging
 from logging.handlers import RotatingFileHandler
@@ -32,7 +31,6 @@ sys.path.append(WAVESHARE_LIB)
 from waveshare_epd import epd7in5h
 
 import renderer
-import api
 
 logger = logging.getLogger(__name__)
 
@@ -41,58 +39,23 @@ def setup():
     epd.init()
     epd.Clear()
     time.sleep(1)
-    
-    api.clear_all()
-    api.refresh_all()
-    
     return epd
-
-def sleep_until_next_minute(offset_seconds=0):
-    now = datetime.now()
-    seconds = now.second + now.microsecond / 1_000_000
-
-    # sleep until (60 - offset)
-    sleep_time = 60 - seconds - offset_seconds
-
-    if sleep_time < 0:
-        sleep_time += 60  # handle wrap-around
-
-    time.sleep(sleep_time)
-    
-def handle_keyboard_interupt(epd):
-    epd.Clear()
-    epd.sleep()
-    logger.info("Display Slept")
-    exit()
     
 def test_render():
-    data = api.return_data()
-    image = renderer.render_dashboard(data)
+    image = renderer.render_dashboard()
     image.save("dashboard.png")
 
 def test_display():
     epd = setup()
-    data = api.return_data()
-    image = renderer.render_dashboard(data)
+    image = renderer.render_dashboard()
     epd.display(epd.getbuffer(image))
 
 if __name__ == "__main__":
     epd = setup()
     logger.info("Dashboard Started")
 
-    while True:
-        try:
-            sleep_until_next_minute()
-            
-            now = datetime.now()
-
-            if now.minute == 0:
-                api.cache["weather"].get(force_refresh=True)
-
-            api.refresh_all()
-            data = api.return_data()       
-            image = renderer.render_dashboard(data)
-            epd.display(epd.getbuffer(image))
-        except KeyboardInterrupt as e:
-            logger.warning("Detected Keyboard Interrupt")
-            handle_keyboard_interupt(epd)
+    image = renderer.render_dashboard()
+    epd.display(epd.getbuffer(image))
+    
+    epd.sleep()
+    logger.info("Display Slept")
